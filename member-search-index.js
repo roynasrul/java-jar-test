639f3d2da57f27543bcd5d22f99e9aada930e409
roynasrul <roynasrul@users.noreply.github.com>
2023-03-10 04:38:58 +0000

    deploy: ab5f92e0855b6d347de3ca02a4fbc78a9a98206a

---
+++ member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.oi.app","c":"App","l":"App()","u":"%3Cinit%3E()"},{"p":"com.oi.app","c":"App","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
